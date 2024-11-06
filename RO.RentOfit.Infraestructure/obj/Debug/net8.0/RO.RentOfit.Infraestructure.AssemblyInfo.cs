ed31e5421602718a76e5c2213827d94b6072bd13
Moisés HC <genesis2204gamerhc@gmail.com>
2024-11-06 16:13:23 -0600

    RO-Sprint 6_Moises: Actualización de la función para consultar vestimentas de un establecimiento

--- RO.RentOfit.Infraestructure/obj/Debug/net8.0/RO.RentOfit.Infraestructure.AssemblyInfo.cs
+++ RO.RentOfit.Infraestructure/obj/Debug/net8.0/RO.RentOfit.Infraestructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RO.RentOfit.Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10982849868419a6c6494c05cb5d9678352183c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70b64b7198d6edfb7980d63db34c7dc596be8519")]
 [assembly: System.Reflection.AssemblyProductAttribute("RO.RentOfit.Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RO.RentOfit.Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
